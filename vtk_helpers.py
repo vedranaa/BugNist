e0446b234d6f019e75d3eefdef76af4fa2fd36ac
Vedrana Andersen Dahl <vedranaa@gmail.com>
2022-12-16 19:58:07 +0100

    moved code to vtk_helpers

--- vtk_helpers.py
+++ vtk_helpers.py
@@ -1,6 +1,7 @@
 import vtk
 import PIL.Image
 import io
+import numpy as np
 
 
 def save_screenshot(filename, renderWindow):
@@ -220,3 +221,29 @@ def save_view_screenshots(renderWindow, name, dims):
         camera.SetViewUp(*viewUp)
         renderer.ResetCamera() # positions camera along view plane normal such that all actors are visible
         save_screenshot(f'{name}_w{n}.png', renderWindow)
+
+def threshold_otsu(image=None, nbins=256):
+    """Return threshold value based on Otsu's method. Adapted from
+    https://github.com/scikit-image/scikit-image/blob/70fa904eee9ef370c824427798302551df57afa1/skimage/filters/thresholding.py#L312
+    """
+
+    counts, bin_edges = np.histogram(image, nbins)
+    bin_centers = 0.5*(bin_edges[:-1] + bin_edges[1:])
+
+    # class probabilities for all possible thresholds
+    weight1 = np.cumsum(counts)
+    weight2 = np.cumsum(counts[::-1])[::-1]
+    
+    # class means for all possible thresholds
+    mean1 = np.cumsum(counts * bin_centers) / weight1
+    mean2 = (np.cumsum((counts * bin_centers)[::-1]) / weight2[::-1])[::-1]
+
+    # Clip ends to align class 1 and class 2 variables:
+    # The last value of ``weight1``/``mean1`` should pair with zero values in
+    # ``weight2``/``mean2``, which do not exist.
+    variance12 = weight1[:-1] * weight2[1:] * (mean1[:-1] - mean2[1:]) ** 2
+
+    idx = np.argmax(variance12)
+    threshold = bin_centers[idx]
+
+    return threshold
